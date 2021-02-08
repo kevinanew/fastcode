0fd2ce2e08688a4bb19245b4af30940895ed0fe8
kevinanew <kevinanew@gmail.com>
2021-02-08 20:13:31 +0800

    update 2021-02-08 20:13:31

--- util/config_file.py
+++ util/config_file.py
@@ -1,13 +1,26 @@
 import os
+import shutil
 
 import yaml
 
 
 def load_config(filename):
-    os.getcwd()
     yaml_file = open(os.path.join(os.getcwd(), filename), 'r')
     yaml_content = yaml.safe_load(yaml_file)
     return yaml_content
+
+
+def build_config(filename):
+    config_template = os.path.join(os.path.dirname(__file__), filename)
+
+    if not os.path.exists(config_template):
+        print(f'配置模版不存在: {config_template}')
+        raise SystemExit
+
+    config_path = os.path.join(os.getcwd(), filename)
+    if not os.path.exists(config_path):
+        print(f'创建配置文件: {filename}')
+        shutil.copy(config_template, config_path)
 
 
 if __name__ == '__main__':
