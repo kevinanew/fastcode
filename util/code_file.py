464d99f0865f32f05c829d6999db9a790cfbf201
kevinanew <kevinanew@gmail.com>
2021-03-04 01:40:40 +0800

    update 2021-03-04 01:40:40

--- util/code_file.py
+++ util/code_file.py
@@ -5,7 +5,9 @@ def add_python_code(file_path, code):
     print(f'path: {file_path}')
     print(code)
     code_file_path = os.path.join(os.getcwd(), file_path)
-    assert os.path.exists(code_file_path)
+    print('Open', code_file_path)
+    if not os.path.exists(code_file_path):
+        open(code_file_path, 'w').close()
 
     content = open(code_file_path, 'r').read()
     if code in content:
